6fefea92da6deaac9114b911d144b8b5b145aca5
pnunn15 <pnunnl@aol.com>
2026-03-08 13:04:22 -0600

    Finalized Stage 1 functionality

--- instr_mem.vhdl
+++ instr_mem.vhdl
@@ -14,17 +14,17 @@ architecture Behavioral of instr_mem is
     signal memory : memory_array := (
         -- used https://www.binaryhexconverter.com/ to convert from binary to hex
         -- used https://www.exploringbinary.com/twos-complement-converter/ to convert from decimal to 2s complement
-        0 => x"00900293", -- addi x5, x0, 9         000000001001 00000 000 00101 0010011
---        1 => x"00429293", -- subi x5, x5, 4
---        2 => x"FFC29293", -- subi x5, x5, -4
+        0 => x"00900293", -- addi x5, x0, 9 | 000000001001 00000 000 00101 0010011
+--        1 => x"00429293", -- subi x5, x5, 4 | 
+--        2 => x"FFC29293", -- subi x5, x5, -4 | 
         1 => x"00000317", -- load_addr x6, array (custom instruction), where array is 0x10000000
         2 => x"00032383", -- lw x7, 0(x6)           
         3 => x"00430313", -- loop: addi x6, x6, 4   
         4 => x"00032503", -- lw x10, 0(x6)    
         5 => x"007503B3", -- add x7, x10, x7 
         6 => x"00129293", -- subi x5, x5, 1 (or "addi x5, x5, -1")  
-        7 => x"FA0298E3", -- bne x5, x0, loop   1 [jump -40 because it will divide it by 2; note: assumes PC is incremented by 4]
-        8 => x"FFDFF06F", -- done: j done             [jump -4; note: assumes PC is incremented by 4]               
+        7 => x"FA0298E3", -- bne x5, x0, loop [jump -40 because it will divide it by 2; note: assumes PC is incremented by 4] | 
+        8 => x"FF9FF06F", -- done: j done [jump -4; note: assumes PC is incremented by 4] | 1 1111111100 1 11111111 00000 1101111
         others => (others => '0')
     );
 begin
